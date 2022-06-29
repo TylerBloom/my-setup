30014e0ad079475a2f0d3e6a464066feb5a37cf6
TylerBloom <tylerbloom2222@gmail.com>
2022-06-29 11:44:26 -0400

    Adjusted dunst install

--- dunst/get_deps.sh
+++ dunst/get_deps.sh
@@ -2,11 +2,11 @@
 
 # Gets the deps for the dunst config
 
-sudo apt install libxinerama-dev libxrandr-dev libxss-dev libglib2.0-dev lib-cairo2-dev libnotify-dev xdg-utils
+sudo apt install libxinerama-dev libxrandr-dev libxss-dev libglib2.0-dev libghc-pango-dev libnotify-dev xdg-utils
 
 wget https://github.com/dunst-project/dunst/archive/refs/tags/v1.9.0.zip
-unzip dunst-1.9.0.zip
-cd dunst-1.9.0
+unzip v1.9.0.zip
+cd v1.9.0
 
 make WAYLAND=0
 sudo make WAYLAND=0 install
